2fac1a15a2de9a7a7c372512729be10ba1022187
Borball Zhai <borball.zh@gmail.com>
2025-04-07 17:22:20 -0400

    eadd exec

--- diff.sh
+++ diff.sh
@@ -64,7 +64,7 @@ compare(){
       rel_path="namespaces/$rel_path"
     fi
 
-    source_cr="$(find )"
+    source_cr="$source_dir/$rel_path"
     target_cr="$target_dir/$rel_path"
 
     exists_source="yes"
